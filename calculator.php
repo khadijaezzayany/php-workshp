e13936a08b73c31d38409aa3d471457594583682
khadijaezzayany <khadijaezzayany8@gmail.com>
2020-07-04 15:35:26 +0100

    solution statement 4

--- calculator.php
+++ calculator.php
@@ -16,7 +16,54 @@
             return $nb * factorielle($nb - 1);
         }
     }
-    echo factorielle(19);
+    echo factorielle(8).'<br>';
+
+
+
+
+
+    function nbrParamètres(...$nbrs){
+        return func_num_args();
+    }
+    $result  = nbrParamètres(1,1,1,1,1,'abcd','abc','azert');
+    echo 'la somme des nombres d\'entrée est:'.$result .'<br>';
+
+
+
+   
+   
+   
+    function bnrPremier(int $number){
+
+        if($number < 2){
+            return false;
+        }
+
+    for ($i = 2; $i <($number -1); $i++){
+       if($number %$i ==0){
+           return false;
+       } 
+    }
+    return true;
+    }
+    $resultt = bnrPremier(97);
+    if($resultt){
+        echo 'le nombre est premier'.'<br>';
+    }
+    else{
+        echo 'le nombre n\'est pas premier '.'<br>';
+    }
+
+
+
+
+    function performOperation(){
+        echo call_user_func('factorielle');
+        echo call_user_func('nbrParamètres');
+        echo call_user_func('bnrPremier');
+    }
+    echo performOperation();
+  
 ?>
    
     
